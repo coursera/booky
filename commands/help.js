dc2cab47b758c16e2ea11a7c646f483a4929bcc5
eleith <online+git@eleith.com>
2016-05-14 01:55:13 -0700

    fix help

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const path = require('path');
 
 const help = new Command(/(help)|[?]/, function(slack) {
-  const tokenized = /help(?:\s+([^\s]+))?/.exec(slack.text.trim());
+  const tokenized = /(?:(?:help)|[?])(?:\s+([^\s]+))?/.exec(slack.text.trim());
   const command = tokenized[1];
   const fullHelp = !command;
 
